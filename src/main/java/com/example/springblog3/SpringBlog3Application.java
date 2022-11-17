62ef49c66041b767ab577a08b66ff5c417bb4bb3
Casanova Geary <CasanovaGeary@Gmail.com>
2022-11-17 14:48:58 -0600

    No fucking help after telling an instructor I'm having problems.

---
+++ src/main/java/com/example/springblog3/SpringBlog3Application.java
@@ -0,0 +1,13 @@
+package com.example.springblog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlog3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlog3Application.class, args);
+    }
+
+}
